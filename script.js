92962364e331c9a6a10adff8b6f83207a82ec588
Ilana <ilana@Justins-Macbook.lan>
2022-07-28 22:30:25 -0500

    draft

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-// Write your JavaScript code here!
+
+
+// const { pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
+require("./scriptHelper")
 
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+        let pilotNameInput = document.querySelector("input[name=pilotName]");
+        let copilotNameInput = document.querySelector("input[name=copilotName]");
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let cargoMassInput = document.querySelector("input[name=cargoMass");
+        let list = document.querySelector("input [name=faultyItems");
+        formSubmission(document, list, pilotNameInput, copilotNameInput, fuelLevelInput, cargoMassInput);
+    });
 
-   let listedPlanets;
+   
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
+       listedPlanets = result; 
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+        let planet = pickPlanet(listedPlanets);
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let imageUrl = planet.image;
+        let moons = planet.moons;
+        addDestinationInfo(document, name, diameter, star, distance, imageUrl, moons)
+    })
    
 });
